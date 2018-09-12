2582733ba34eeba31b01c7e5d1d08393d5235e19
HanXu <601042507@qq.com>
2018-09-12 20:51:46 +0800

    继承---子类与父类的构造顺序；编译出错，Animal父类没有构造函数，导致子类也没有构造函数

---
+++ inherit-2.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+	> File Name: inherit-2.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 三  9/12 20:44:04 2018
+ ************************************************************************/
+#include <iostream>
+#include <string.h>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+struct Animal{
+    Animal(string name) : name(name){}
+private:
+    string name;
+};
+
+struct Bat : public Animal{
+
+};
+
+int main(){
+    Bat b;
+    return 0;
+}
