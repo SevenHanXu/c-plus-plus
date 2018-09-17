a2d8d0bfc1932b01b2b88a70b9f964d313b11e19
HanXu <601042507@qq.com>
2018-09-17 10:51:46 +0800

    构造函数

--- test.cpp
+++ test.cpp
@@ -15,22 +15,43 @@ using std::endl;
 class P{
     private:
         int a;
+        string m_name;
     public:
+        P(){
+            a = 0;
+            cout << "P() " << endl;
+        }//函数返回值没有，构造函数名必须和类名相等i；
+         P(string name){
+             cout << "P(string)" << endl;
+            this->m_name = name;
+        }
         void setValue(int v);
         int getValue();
+        string getname();
 };
 void P::setValue(int v){
     this->a = v;
     return ;
 }
 
-int P:: getValue(){
+int P::getValue(){
     return this->a;
 }
 
+string P::getname(){
+    return this->m_name;
+}
+
 int main(){
-    P p;
-    p.setValue(6);
-    cout << p.getValue() << endl;
+    int *m = new int(1);
+    int *a = new int [10];
+    delete m;
+    delete[] a;
+    P *p = new P;
+    P *p1 = new P("wrf");
+    P *p2 = new P("fbq");
+    cout << p1->getname() << " " << p2->getname() << endl;
+    //P p;//栈
+    //P *p1 = new P;//堆上，malloc也是在堆上；
     return 0;
 }
