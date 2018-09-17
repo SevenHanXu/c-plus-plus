a0635c676904908b57bee8dd2eebfb9acbde8d36
HanXu <601042507@qq.com>
2018-09-17 11:41:01 +0800

    拷贝构造函数

--- test.cpp
+++ test.cpp
@@ -16,6 +16,7 @@ class P{
     private:
         int a;
         string m_name;
+        int *data;
     public:
         P(){
             a = 0;
@@ -25,9 +26,16 @@ class P{
              cout << "P(string)" << endl;
             this->m_name = name;
         }
+        P(const P& obj){
+            a = obj.a;
+            m_name = obj.m_name;
+            data = new int;
+            *data = *obj.data;
+        }
         void setValue(int v);
         int getValue();
         string getname();
+        int *getADD();
 };
 void P::setValue(int v){
     this->a = v;
@@ -41,16 +49,14 @@ int P::getValue(){
 string P::getname(){
     return this->m_name;
 }
-
+int *P::getADD(){
+    return data;
+}
 int main(){
-    int *m = new int(1);
-    int *a = new int [10];
-    delete m;
-    delete[] a;
-    P *p = new P;
-    P *p1 = new P("wrf");
-    P *p2 = new P("fbq");
-    cout << p1->getname() << " " << p2->getname() << endl;
+    P p("wrf");
+    P p1(p);
+    cout << p.getname() << " " << p1.getname() << endl;
+    cout << p.getADD() << " " << p1.getADD() << endl;
     //P p;//栈
     //P *p1 = new P;//堆上，malloc也是在堆上；
     return 0;
