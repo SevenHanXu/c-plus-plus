56c7f892ccdaec610581af33b509a5f1955314ff
HanXu <601042507@qq.com>
2018-09-28 10:58:40 +0800

    bool类型的值是0或1，非0的值，哪怕是负数的bool值都是1

---
+++ bool_test.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: bool_test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 五  9/28 10:56:31 2018
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(){
+    bool a = true;
+    a = -1;
+    cout << "a = " << a << endl;
+    a = 0;
+    cout << "a = " << a << endl;
+    return 0;
+}
