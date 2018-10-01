3291c258cdf6fd94f4c8f87807d2bf0b1c164d06
HanXu <601042507@qq.com>
2018-10-01 16:16:50 +0800

    日常练手7---运算符重载

--- copyfun.cpp
+++ copyfun.cpp
@@ -15,9 +15,19 @@ public:
 	People(const People &a);
 	~People();
 	void out();
-	People operator = (const People &a){//返回&是为了减少拷贝操作；
-		cout << this << "operator = is called" << endl;
-		return *this;//提领操作，把里面的值拿出来
+	People operator = (People a){//返回&是为了减少拷贝操作；
+		a.x = "you are 250";
+		cout << this << "operator1 = is called" << endl;
+		return a;//提领操作，把里面的值拿出来
+	}
+	int operator = (const int x){
+		if(x == 1) {
+			this->x = "100";
+		}else{
+			this->x = "200";
+		}
+		cout << this << "operator2 = is called" << endl;
+		return 256;
 	}
 private:
 	string x;
@@ -33,10 +43,7 @@ People func(){
 int main(){
 	People a("hug");
 	People b = func();
-	a.out();
-	b.out();
-	(a = b).out();
-	a.out();
+	a = (a = b);//(1)b给a赋值，最后还要给()外的a赋值，所以要返回一个切切实实的对象，有一个拷贝构造并调用opreator1;(2)(a = b)的结果给a赋值，需要一次拷贝并调用opreator1(3)最后得到a之后也要返回一个对象，再拷贝一次
 	return 0;
 }
 
