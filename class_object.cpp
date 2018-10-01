08ad7c1cdb57edb6074fe8ee16e30410245a21cf
HanXu <601042507@qq.com>
2018-10-01 14:02:12 +0800

    日常练手2---在类外用构造函数的时候一共做了两次赋值，多进行了一次赋值

---
+++ class_object.cpp
@@ -0,0 +1,35 @@
+/*************************************************************************
+	> File Name: class_object.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 一 10/ 1 13:59:35 2018
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+class People{
+public:
+	People(string name);
+	void say(string word){
+		cout << this->__name << " say : " << word << endl;
+	}
+	void set_name(string __name){
+		this->__name = __name;
+	}
+private:
+	string __name;
+};
+
+
+
+People::People(string name){
+	this->__name = name;
+}
+
+
+int main(){
+	People hanxu("hanxu");
+	hanxu.say("I love you!");
+	return 0;
+}
