19e0c35d83a111b39441da68842cb62df0e0417c
HanXu <601042507@qq.com>
2018-10-04 19:36:58 +0800

    多态---在实现字符型哈希表的基础上，利用vitual实现自定义类型的哈希函数

--- many_status2.cpp
+++ many_status2.cpp
@@ -15,12 +15,16 @@ using std::string;
 
 class HashFunc{
 public:
-	virtual int operator()(const char *str) const{//const对象只能用const方法；
+	virtual int operator()(const void *data) const{//const对象只能用const方法；
+		const char *str = static_cast<const char *>(data);
 		int hash = 0;
 		for(int i = 0; str[i]; i++){
 			hash = (hash << 5) ^ str[i] ^ (hash >> 3);
 		}
 		return (hash & 0x7fffffff);
+	}
+	virtual int operator()(int data) const{
+		return data;
 	}
 };
 
@@ -28,8 +32,11 @@ public:
 class HashTable{
 public:
 	HashTable(const HashFunc &func) : __func(&func){}
-	int get_hash(const char *str){
-		return (*(this->__func))(str);
+	int get_hash(const void *data){
+		return (*(this->__func))(data);
+	}
+	int get_hash(int data){
+		return (*(this->__func))(data);
 	}
 private:
 	const HashFunc *__func;
@@ -37,7 +44,8 @@ private:
 
 class my_HashFunc : public HashFunc{
 public:
-	int operator()(const char *str) const override{
+	int operator()(const void *data) const override{
+		const char *str = static_cast<const char *>(data);
 		int hash = 0;
 		for(int i = 0; str[i]; i++){
 			hash = (hash << 7) ^ str[i] ^ (hash >> 5);
@@ -46,12 +54,22 @@ public:
 	}
 };
 
+class my_HashFunc2 : public HashFunc{
+public:
+	int operator()(int data) const override{
+		return data << 3;
+	}
+};
+
 int main(){
 	HashFunc hf;
 	HashTable ht(hf);
 	my_HashFunc hf2;
 	HashTable ht2(hf2);
+	my_HashFunc2 hf3;
+	HashTable ht3(hf3);
 	cout << ht.get_hash("haizei") << endl;
 	cout << ht2.get_hash("haizei") << endl;
+	cout << ht3.get_hash(678) << endl;
 	return 0;
 }
