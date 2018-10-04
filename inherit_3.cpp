7c1d5b7b774e10dfcaa17911f48bd165f3171b77
HanXu <601042507@qq.com>
2018-10-04 16:31:55 +0800

    继承---构造顺序详解

--- inherit_3.cpp
+++ inherit_3.cpp
@@ -14,7 +14,9 @@ using std::string;
 
 class Animal{
 public:
-	Animal(string name) : __name(name){}
+	Animal(string name) : __name(name){
+		cout << "Animal constructor" << endl;
+	}
 	void set_name(string name){
 		this->__name = name;
 	}
@@ -27,7 +29,12 @@ protected:
 
 class Cat : protected Animal {
 public:
-	Cat(string name) : Animal(name){}
+	Cat() : Animal("temp_name"){
+		cout << "Cat constructor" << endl;
+	}
+	Cat(string name) : Animal(name){
+		cout << "Cat constructor" << endl;
+	}
 	void tell_me_your_name(){
 		cout << "cat name is : " << this->__name << endl;
 	}
@@ -35,11 +42,22 @@ private:
 	int tail;
 };
 
+
+class Tail{
+public:
+	Tail(int length) :__length(length){}
+private:
+	int __length;
+};
+
 class HasTailCat : public Cat{
 public:
-	HasTailCat(string name) : Cat(name){
-		
+	HasTailCat(string name) : tail(6){
+		cout << "HasTailCat constructor" << endl;
+		this->__name = name;
 	}
+private:
+	Tail tail;
 };//覆盖继承，就近；
 
 int main(){
@@ -47,3 +65,8 @@ int main(){
 	a.tell_me_your_name();
 	return 0;
 }
+//构造函数调用顺序:(1)HasTailCat(2)Cat(3)Animal;
+//构造函数完成顺序(1)Animal(2)Cat(3)HasTailCat;
+//构造函数时，先去执行初始化列表，然后再去完成构造;
+//父类的属性由父类进行构造
+
